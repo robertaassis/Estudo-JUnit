ba3928dba9b628a1329d830a5012d962e26d7ab0
Roberta Assis de Carvalho <roberta.assisdecarvalho9@gmail.com>
2022-03-14 15:43:43 -0300

    JUnit e testes unitários - corrigidos

--- src/main/java/br/com/alura/tdd/service/BonusService.java
+++ src/main/java/br/com/alura/tdd/service/BonusService.java
@@ -3,6 +3,7 @@ package main.java.br.com.alura.tdd.service;
 import main.java.br.com.alura.tdd.modelo.Funcionario;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class BonusService {
 
@@ -11,7 +12,7 @@ public class BonusService {
 		if (valor.compareTo(new BigDecimal("1000")) > 0) { // se o valor for maior do que 1000
 			valor = BigDecimal.ZERO; // altera o valor do bonus pra 0
 		}
-		return valor;
+		return valor.setScale(2, RoundingMode.HALF_UP);
 	}
 
 }
--- src/main/java/br/com/alura/tdd/service/BonusServiceTest.java
+++ src/main/java/br/com/alura/tdd/service/BonusServiceTest.java
@@ -12,18 +12,21 @@ public class BonusServiceTest {
     void bonusDeveriaSerZeroParaSalarioAlto(){ // teste para que se o salario for alto, zere ele
         BonusService service = new BonusService();
         BigDecimal bonus = service.calcularBonus(new Funcionario("Rodrigo", LocalDate.now(), new BigDecimal("11000")));
-        assertEquals(BigDecimal.ZERO, bonus);
+        assertEquals(new BigDecimal("0.00"), bonus);
     }
 
+    @Test
     void bonusDeveriaSer10PorCentodoSalario(){ // teste para trazer o bonus de 10% do salario para salarios baixos
         BonusService service = new BonusService();
         BigDecimal bonus = service.calcularBonus(new Funcionario("Rodrigo", LocalDate.now(), new BigDecimal("2500")));
-        assertEquals(250, bonus);
+        assertEquals(new BigDecimal("250.00"), bonus);
     }
 
+    @Test
     void bonusDeveriaSer1000(){ // meio termo
         BonusService service = new BonusService();
         BigDecimal bonus = service.calcularBonus(new Funcionario("Rodrigo", LocalDate.now(), new BigDecimal("10000")));
-        assertEquals(1000, bonus);
+        assertEquals(new BigDecimal("1000.00"), bonus);
     }
+    
 }

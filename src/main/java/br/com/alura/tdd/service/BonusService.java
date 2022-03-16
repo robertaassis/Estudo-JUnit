6471b65c941b1aa9612bb137a1d5821ae6ed2af1
Roberta Assis de Carvalho <roberta.assisdecarvalho9@gmail.com>
2022-03-16 12:11:57 -0300

    JUnit com exceptions

--- src/main/java/br/com/alura/tdd/service/BonusService.java
+++ src/main/java/br/com/alura/tdd/service/BonusService.java
@@ -9,8 +9,8 @@ public class BonusService {
 
 	public BigDecimal calcularBonus(Funcionario funcionario) {
 		BigDecimal valor = funcionario.getSalario().multiply(new BigDecimal("0.1")); // multiplica salario por 0.1
-		if (valor.compareTo(new BigDecimal("1000")) > 0) { // se o valor for maior do que 1000
-			valor = BigDecimal.ZERO; // altera o valor do bonus pra 0
+		if (valor.compareTo(new BigDecimal("1000")) > 0) { // se o valor for maior do que 1000, não tem bonus
+			throw new IllegalArgumentException("Funcionario com salário maior do que R$10000 não pode receber bônus!"); // chegando um funcionario ilegal (salario muito alto)
 		}
 		return valor.setScale(2, RoundingMode.HALF_UP);
 	}
--- src/main/java/br/com/alura/tdd/service/BonusServiceTest.java
+++ src/main/java/br/com/alura/tdd/service/BonusServiceTest.java
@@ -5,14 +5,22 @@ import main.java.br.com.alura.tdd.modelo.Funcionario;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class BonusServiceTest {
     @Test
-    void bonusDeveriaSerZeroParaSalarioAlto(){ // teste para que se o salario for alto, zere ele
+    void bonusDeveriaSerErroParaSalarioAlto(){ // teste para que se o salario for alto, devolve exception
         BonusService service = new BonusService();
-        BigDecimal bonus = service.calcularBonus(new Funcionario("Rodrigo", LocalDate.now(), new BigDecimal("11000")));
-        assertEquals(new BigDecimal("0.00"), bonus);
+        assertThrows(IllegalArgumentException.class, () -> service.calcularBonus(new Funcionario("Rodrigo", LocalDate.now(), new BigDecimal("11000"))));
+        // checa se o a expection que está devolvendo é um IllegalArgument e precisa de um lambda com o método para o segundo argumento
+
+        // ABAIXO É OUTRA FORMA DE FAZER O ASSERTTHROWS
+        /* try{
+            service.calcularBonus(new Funcionario("Rodrigo", LocalDate.now(), new BigDecimal("11000")));
+            fail("Não gerou a exception"); // se entrar nessa linha, o teste falhou, porque já era pra jogar exception na linha de cima e cair direto no catch, nao nessa linha aqui//
+        } catch(Exception e){ // é o que estou esperando; checa se a mensagem de falha é igual
+            assertEquals("Funcionario com salário maior do que R$10000 não pode receber bônus!", e.getMessage());
+        } */
     }
 
     @Test
